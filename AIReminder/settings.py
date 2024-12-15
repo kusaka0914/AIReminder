01d4c75d2b4d84541ce911dcff3b79aaab7d1efc
日下拓海 <kusakatakumi@kusakatakuminoMacBook-Pro.local>
2024-12-15 22:30:04 +0900

    移植

--- AIReminder/settings.py
+++ AIReminder/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,10 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # 静的ファイルを集める際の出力先
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 開発環境での静的ファイルの検索パス
 STATICFILES_DIRS = [
