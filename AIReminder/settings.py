106e31053a166c0477a75c55c178ca0b784aae07
日下拓海 <kusakatakumi@kusakatakuminoMacBook-Pro.local>
2024-12-16 13:50:06 +0900

    a

--- AIReminder/settings.py
+++ AIReminder/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&-6gih)r1%%313^o3+gfo$^=6*38123ur_-1z(@3f&84m3!*v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['generaite-7ef7b582b2c7.herokuapp.com', '127.0.0.1', 'localhost']
